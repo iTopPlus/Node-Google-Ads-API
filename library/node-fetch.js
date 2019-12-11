3b0d3c233f17cdd0e3d3c8d824dff18b992ff3b1
Flintlozk <itopplus.mkt@gmail.com>
2019-12-11 18:05:51 +0700

    Handler Error

--- library/node-fetch.js
+++ library/node-fetch.js
@@ -1,12 +1,11 @@
 const fetch = require('node-fetch')
-const { FetchError } = require('../errors')
 
 const NodeFetch = async (endpoint, option) => {
   try {
     const response = await fetch(endpoint, option)
     return response
   } catch (err) {
-    throw new FetchError(err)
+    throw new Error(err)
   }
 }
 
